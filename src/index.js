28782b914178bbb0d4d1edbc1fb45aedc67a8e2a
Josh Burd <Jburdt@gmail.com>
2023-05-02 16:55:06 -0400

    partially set up

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { thunkMiddleware } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import catsReducer from "./features/cats/catsSlice";
+import { Provider } from "react-redux";
+
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+
+const store = createStore(catsReducer, composedEnhancer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />, 
+  </Provider>,
+document.getElementById("root"));
